b0c7f805769493e3c1f26fb6c36f3a5e7d719bf1
Zach Stocker <zacharystocker@gmail.com>
2025-05-11 22:01:01 -0500

    some code

--- src/main/java/com/ender/communitydayspawner/TimedSpawnInstance.java
+++ src/main/java/com/ender/communitydayspawner/TimedSpawnInstance.java
@@ -22,7 +22,7 @@ public class TimedSpawnInstance {
     }
 
     public void trySpawn(ServerWorld world) {
-        if (world.getRandom().nextFloat() < 0.10f) { // 10% chance per tick
+        if (world.getRandom().nextFloat() < 0.25f) { // 25% chance per tick
             TimedSpawnManager.spawnPokemon(world, species, origin);  // Use the stored origin for spawn
         }
     }
